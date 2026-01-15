4d08e31e5611639b3bfde750dc52b4aacfa28ba2
Pedro Magno <pedromagno04@gmail.com>
2026-01-15 16:14:06 -0300

    Feat: Ultima pr do sistema se deus quiser

--- projeto/settings.py
+++ projeto/settings.py
@@ -75,18 +75,16 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",  # modern alias
-        "NAME": "bdibs",
-        "USER": "postgres",
-        "PASSWORD": "coti",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-"""
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",  # modern alias
+#         "NAME": "bdibs",
+#         "USER": "postgres",
+#         "PASSWORD": "coti",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 DATABASES = {
     "default": {
